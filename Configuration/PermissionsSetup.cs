7716cd3831d57b8239670a1d3f18c0df59b6f9ce
grammophone <emmanuel.liossis@gmail.com>
2017-03-25 23:56:33 +0200

    Set PermissionsSetup.PermissionAssignments property as lazily created.

--- Configuration/PermissionsSetup.cs
+++ Configuration/PermissionsSetup.cs
@@ -12,9 +12,31 @@ namespace Grammophone.Domos.AccessChecking.Configuration
 	[Serializable]
 	public class PermissionsSetup
 	{
+		#region Private fields
+
+		private PermissionAssignments permissionAssignments;
+
+		#endregion
+
+		#region Public properties
+
 		/// <summary>
 		/// The collection of permissions along with their assignments.
 		/// </summary>
-		public PermissionAssignments PermissionAssignments { get; set; }
+		public PermissionAssignments PermissionAssignments
+		{
+			get
+			{
+				return permissionAssignments ?? (permissionAssignments = new PermissionAssignments());
+			}
+			set
+			{
+				if (value == null) throw new ArgumentNullException(nameof(value));
+
+				permissionAssignments = value;
+			}
+		}
+
+		#endregion
 	}
 }
